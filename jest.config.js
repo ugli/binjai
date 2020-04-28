6a3b811a0ba6d0abeeae6e1195ea66355d6e1fff
Fredrik Kivi <fredrik.kivi@ugli.se>
2020-04-28 22:14:04 +0200

    testing with jest added

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  testMatch: [
+    "**/src/test/typescript/**/*.ts"
+ ],
+};
---
+++ src/test/typescript/collection/OptionTest.ts
@@ -0,0 +1,19 @@
+import {Options} from "../../../main/typescript/collection/Option";
+
+describe("Option", () => {
+    it("Some number", () => {
+      expect(Options.from(3).orElse(2)).toEqual(3);
+    });
+    it("None number", () => {
+      expect(Options.from(undefined).orElse(2)).toEqual(2);
+    });
+    it("Map to string", () => {
+      expect(Options.from(3).map(e => `a${e}`).orElse("")).toEqual("a3");
+    });
+    it("Filter to None", () => {
+      expect(Options.from(3).filter(e => false).orElse(-1)).toEqual(-1);
+    });
+    it("Filter to Some", () => {
+      expect(Options.from(3).filter(e => true).orElse(-1)).toEqual(3);
+    });
+  });
